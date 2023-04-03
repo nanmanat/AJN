863af0b6fd454f72ff3f8191fa33353c2472c810
sSerpeD <80467475+sSerpeD@users.noreply.github.com>
2023-04-04 02:17:57 +0700

    aaa

--- entity/Entity.java
+++ entity/Entity.java
@@ -30,6 +30,9 @@ public class Entity {
         this.gp = gp;
     }
 
+    public void setAction() { }
+    public void update() { }
+
     public void draw(Graphics2D g2) {
         BufferedImage image = null;
         int screenX = worldX - gp.player.worldX + gp.player.screenX;
